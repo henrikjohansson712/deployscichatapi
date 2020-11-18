052d5f5f6235e26324a0c47c48aabe2c6a3a51ab
henrikjohansson712 <henrik.johansson712@gmail.com>
2020-11-18 10:54:29 +0100

    Update deployment

---
+++ secret.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat08.esss.lu.se" -days 3650
+	kubectl create secret -ndev tls scichatservice --key tls.key --cert tls.crt
+	rm tls.*
+
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat07.esss.lu.se" -days 3650
+	kubectl create secret -nqa tls scichatservice --key tls.key --cert tls.crt
+	rm tls.*
+
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scitest.esss.lu.se" -days 3650
+	kubectl create secret -nproduction tls scichatservice --key tls.key --cert tls.crt
+	rm tls.*
