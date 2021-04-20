37cf90b73c5bc84bab73003236e6ddf8954f14a5
Henrik Johansson <henrik.johansson712@gmail.com>
2021-04-20 10:14:10 +0200

    Update secrets script

--- secret.sh
+++ secret.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat08.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scitest.esss.lu.se" -days 3650
 	kubectl create secret -ndev tls scichatservice --key tls.key --cert tls.crt
 	rm tls.*
 
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat07.esss.lu.se" -days 3650
-	kubectl create secret -nqa tls scichatservice --key tls.key --cert tls.crt
-	rm tls.*
-
-	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scitest.esss.lu.se" -days 3650
+	openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj "/CN=scicat.ess.eu" -days 3650
 	kubectl create secret -nproduction tls scichatservice --key tls.key --cert tls.crt
 	rm tls.*
